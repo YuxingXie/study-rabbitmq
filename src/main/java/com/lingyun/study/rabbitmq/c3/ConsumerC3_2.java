70f66c812644801cbf8003f7bb9a8ed452539b17
xieyuxing <185246042@qq.com>
2022-10-26 15:57:11 +0800

    sss

--- src/main/java/com/lingyun/study/rabbitmq/c3/ConsumerC3_2.java
+++ src/main/java/com/lingyun/study/rabbitmq/c3/ConsumerC3_2.java
@@ -1,20 +1,27 @@
 package com.lingyun.study.rabbitmq.c3;
 
 import com.lingyun.study.rabbitmq.common.RabbitMqUtils;
-import com.rabbitmq.client.*;
+import com.rabbitmq.client.CancelCallback;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.DeliverCallback;
+import com.rabbitmq.client.Delivery;
 
 import java.io.IOException;
 import java.util.concurrent.TimeoutException;
 
-public class ConsumerC3_1 {
+/**
+ * 手动应答方式
+ */
+public class ConsumerC3_2 {
     public static final String QUEUE_NAME="queue_hello_c3";
     public static void main(String[] args) throws IOException, TimeoutException {
 
         Channel channel = RabbitMqUtils.createChannel();
-
-        channel.basicConsume(QUEUE_NAME, true, new DeliverCallback() {
+        //手动应答，第二个参数为false
+        channel.basicConsume(QUEUE_NAME, false, new DeliverCallback() {
             @Override
-            public void handle(String consumerTag, Delivery message) {
+            public void handle(String consumerTag, Delivery message) throws IOException {
+                channel.basicAck(message.getEnvelope().getDeliveryTag(),false);
                 System.out.println("deliverCallback message:" + new String(message.getBody()));
             }
         }, new CancelCallback() {
