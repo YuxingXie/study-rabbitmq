dc5f05942dc3a9f82edb196adb66cc9f5eee2907
xieyuxing <185246042@qq.com>
2022-10-26 21:59:28 +0800

    sss

--- src/main/java/com/lingyun/study/rabbitmq/c3/PublisherC3_1.java
+++ src/main/java/com/lingyun/study/rabbitmq/c3/PublisherC3_1.java
@@ -7,8 +7,7 @@ import java.util.concurrent.TimeoutException;
 
 
 public class PublisherC3_1 {
-    //队列名
-    //发消息
+
     public static void main(String[] args) throws IOException, TimeoutException, InterruptedException {
 
         Channel channel = RabbitMqUtils.createChannel();
